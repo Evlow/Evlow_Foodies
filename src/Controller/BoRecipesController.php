dce95e72e4a4d3284dcc74de56f17920947d5ba1
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2022-06-23 16:25:11 +0200

    update

--- src/Controller/BoRecipesController.php
+++ src/Controller/BoRecipesController.php
@@ -64,5 +64,33 @@ class BoRecipesController extends AbstractController
         ]);
     }
 
+    #[Route('/recette/modifier/{id}', name: 'app_bo_recipes_up')]
+    public function recipetUpdate($id, Request $request, ManagerRegistry $doctrine): Response
+    {      
+        // On recupère le produit sélectionné avec l'ID
+        $recipe = $doctrine->getRepository(Recipes::class)->find($id);
+        // On initialise nos champs dates avec la date d'aujourd'hui
+        $recipe->setUpdatedAt(new DateTimeImmutable());
+        // Etape 01 : Crée une instance de la classe Form à partir de la classe Recipes
+        $formRecipe = $this->createForm(RecipeType::class, $recipe);
+     // Etape 02 : Permet de gérer le traitement de la saisie du formulaire.
+     $formRecipe->handleRequest($request);
+     // Etape 03 : test si le formulaire a été saisi et si les règles de validations sont vérifiées
+     if($formRecipe->isSubmitted() && $formRecipe->isValid())
+     {
+         $entityManager = $doctrine->getManager();
+         // Etape 3.2 : On envoi les données a la bdd
+         $entityManager->flush();
+         // Etape 3.3 : Affichage d'un message succès
+         $this->addFlash('success_product', 'La recette '. $recipe->getTitle(). ' a été modifiée !');
+         // Etape 3.4 : redirection
+         return $this->redirectToRoute('app_home');
+
+         
+     }
+     return $this->render('bo_recipes/recipes.html.twig', [
+        'formRecipe' => $formRecipe->createView()
+    ]);
+   }
 
 }
