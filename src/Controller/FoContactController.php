b48b400b47a9cdb755581cbfa5aa23666ec6f9f6
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2022-06-28 22:32:45 +0200

    service mail

--- src/Controller/FoContactController.php
+++ src/Controller/FoContactController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Doctrine\ORM\EntityManager;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,18 +15,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class FoContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_fo_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid() ){
-        $entityManager->persist($contact);
-        $entityManager->flush();
-        $this->addFlash('succes', 'Votre message a bien été envoyé');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $email = (new Email())
+            ->from($contact->getEmail())
+            ->to('mathilde.peauger@gmail.com')
+            ->subject('Test') 
+            ->html($contact);
+
+            $mailer->send($email);
+            $this->addFlash('succes', 'Votre message a bien été envoyé');
+            
+            return $this->redirectToRoute('app_fo_home');
+       
         }
+
         return $this->render('fo_contact/contact.html.twig', [
+            'contact_email'=>$this->getParameter('app.contact.email'),
             'form' => $form->createView()
         ]);
     }
